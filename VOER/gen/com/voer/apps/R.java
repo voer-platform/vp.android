9ca4b641a49b69419f2dedcf650d199a1f7b259f
v2tmobile <v2tmobile.it@gmail.com>
2014-03-12 23:28:49 +0700

    add menu slide with style; layout about, browse, my document,... add Class GetContent to get json content from server.

--- VOER/gen/com/voer/apps/R.java
+++ VOER/gen/com/voer/apps/R.java
@@ -130,37 +130,71 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int icon_menu_padding=0x7f050005;
+        public static final int menu_padding_bottom=0x7f050007;
+        public static final int menu_padding_left=0x7f050004;
+        public static final int menu_padding_top=0x7f050006;
         public static final int shadow_width=0x7f050002;
         public static final int slidingmenu_offset=0x7f050003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int search_box_style=0x7f020001;
-        public static final int shadow=0x7f020002;
+        public static final int icon_menu_about=0x7f020001;
+        public static final int icon_menu_browser=0x7f020002;
+        public static final int icon_menu_home=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int search_box_style=0x7f020005;
+        public static final int shadow=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f040007;
+        public static final int ImageView02=0x7f04000d;
+        public static final int ImageView03=0x7f04000b;
+        public static final int ImageView04=0x7f040007;
+        public static final int ImageView05=0x7f040009;
+        public static final int TextView02=0x7f04000e;
+        public static final int TextView03=0x7f04000c;
+        public static final int TextView04=0x7f040008;
+        public static final int TextView05=0x7f04000a;
+        public static final int action_settings=0x7f040010;
         public static final int fullscreen=0x7f040003;
         public static final int left=0x7f040000;
         public static final int margin=0x7f040002;
         public static final int right=0x7f040001;
-        public static final int search_box=0x7f040005;
+        public static final int scrollView1=0x7f040005;
+        public static final int search_box=0x7f040006;
         public static final int selected_view=0x7f040004;
-        public static final int slidingmenumain=0x7f040006;
+        public static final int slidingmenumain=0x7f04000f;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int menu=0x7f030001;
-        public static final int slidingmenumain=0x7f030002;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_base=0x7f030001;
+        public static final int activity_browse_online=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_my_document=0x7f030004;
+        public static final int menu=0x7f030005;
+        public static final int slidingmenumain=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int about=0x7f080000;
+        public static final int base=0x7f080001;
+        public static final int browse_online=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int my_document=0x7f080004;
     }
     public static final class string {
+        public static final int about_voer=0x7f06000b;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int menu_about=0x7f060006;
+        public static final int menu_browse=0x7f060007;
+        public static final int menu_home=0x7f060004;
+        public static final int menu_mydocument=0x7f060005;
         public static final int search_hint_text=0x7f060003;
+        public static final int title_activity_about=0x7f06000a;
+        public static final int title_activity_base=0x7f06000c;
+        public static final int title_activity_browse_online=0x7f060009;
+        public static final int title_activity_my_document=0x7f060008;
     }
     public static final class style {
         /** 
@@ -189,6 +223,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        /**  ActionBar styles 
+         */
+        public static final int MyActionBar=0x7f070003;
+        /**  the theme applied to the application or activity 
+         */
+        public static final int VoerTheme=0x7f070002;
     }
     public static final class styleable {
         /** Attributes that can be used with a SlidingMenu.
--- VOER/src/com/voer/apps/MainActivity.java
+++ VOER/src/com/voer/apps/MainActivity.java
@@ -1,5 +1,7 @@
 package com.voer.apps;
 
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +9,7 @@ import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
@@ -14,60 +17,96 @@ import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 public class MainActivity extends Activity {
 	SlidingMenu menu;
 	EditText searchText;
-	@SuppressWarnings("deprecation")
+	GetContent getContent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		Display display = getWindowManager().getDefaultDisplay();
-		
-		menu = new SlidingMenu(this);
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        //menu.setShadowDrawable(R.drawable.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
-        menu.setMenu(R.layout.menu);
-        
-        searchText = (EditText) findViewById(R.id.search_box);
-        Log.e("width: ", String.valueOf(display.getWidth()*0.8));
-        searchText.setWidth((int)(display.getWidth()*0.8)); 
+		showHome();
+		getContent = new GetContent("http://dev.voer.vn:2013/1/materials/?format=json") {
+			
+			@Override
+			void loadFailed() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			void loadCompleted() {
+				// TODO Auto-generated method stub
+				try {
+					Log.e("count: ", getContent.getResponse().get("count").toString());
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
 	}
+
+	private void setMenu() {
+		menu = new SlidingMenu(this);
+		menu.setMode(SlidingMenu.LEFT);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		// menu.setShadowDrawable(R.drawable.shadow);
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		menu.setFadeDegree(0.35f);
+		menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+		menu.setMenu(R.layout.menu);
+	}
+
 	@Override
-    public void onBackPressed() {
-        if ( menu.isMenuShowing()) {
-        	menu.toggle();
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
+	public void onBackPressed() {
+		if (menu.isMenuShowing()) {
+			menu.toggle();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
 	@Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ( keyCode == KeyEvent.KEYCODE_MENU ) {
-            this.menu.toggle();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			this.menu.toggle();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case android.R.id.home:
-            this.menu.toggle();
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.menu.toggle();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void showHome() {
+		setContentView(R.layout.activity_main);
+		Display display = getWindowManager().getDefaultDisplay();
+		setMenu();
+
+		searchText = (EditText) findViewById(R.id.search_box);
+		searchText.setWidth((int) (display.getWidth() * 0.9));
+	}
+
+	public void showHome(View v) {
+		showHome();
+	}
+
+	public void showAbout(View v) {
+		setContentView(R.layout.activity_about);
+		setMenu();
 	}
 
 }
